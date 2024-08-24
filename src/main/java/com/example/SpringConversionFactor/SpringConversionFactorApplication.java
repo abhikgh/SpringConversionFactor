53cd1d10917225342a638982361977dc0379357b
Abhik <abhikgh.work@gmail.com>
2024-08-24 20:19:08 +0530

    update

--- src/main/java/com/example/SpringConversionFactor/SpringConversionFactorApplication.java
+++ src/main/java/com/example/SpringConversionFactor/SpringConversionFactorApplication.java
@@ -2,11 +2,8 @@ package com.example.SpringConversionFactor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class SpringConversionFactorApplication {
 
 	public static void main(String[] args) {
